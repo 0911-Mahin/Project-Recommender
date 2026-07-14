be4412690ac04befca19c778a3f09ad03ca7cda0
Mahin Ahmed Shariff <mahin.s0911@gmail.com>
2026-07-14 08:12:19 +0530

    DisableCORS

--- Django-Backend/Backend/settings.py
+++ Django-Backend/Backend/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "Account",
     "Recommender",
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -53,7 +54,12 @@ REST_FRAMEWORK = {
     )
 }
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
+]
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
